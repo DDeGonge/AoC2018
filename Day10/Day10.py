7a2bc7b04df59f3098fa27bb4a44ba6b906260ad
Andrew Degonge <andrewdegonge@AndrewDegonge-C02WXMCNJG5H.local>
2018-12-12 15:26:16 -0500

    Day 10 Solved

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -21,7 +21,24 @@ def Clean_Data(rawdata):
 		outvec.append(tmp)
 	return np.array(outvec)
 
-def Find_Min(coords, it):
+def Get_Min(cin):
+	tc = cin
+	tc[:,0] += tc[:,2]
+	tc[:,1] += tc[:,3]
+
+	# Shift to 0,0 for first particle
+	xmin = min(tc[:,0])
+	ymin = min(tc[:,1])
+	tc[:,0] += -xmin
+	tc[:,1] += -ymin
+
+	xmax = max(tc[:,0])
+	ymax = max(tc[:,1])
+
+	return xmax, ymax
+
+def Transform_Plt(cin, it):
+	coords = cin
 	coords[:,0] += it*coords[:,2]
 	coords[:,1] += it*coords[:,3]
 
@@ -31,10 +48,28 @@ def Find_Min(coords, it):
 	coords[:,0] += -xmin
 	coords[:,1] += -ymin
 
-	xmax = max(coords[:,0])
-	ymax = max(coords[:,1])
+	return coords
 
-	return xmax, ymax, coords
+def Plot_Points(cin):
+	try:
+		ymax = max(cin[:,0]) + 1
+		xmax = max(cin[:,1]) + 1
+
+		ar = np.zeros((xmax, ymax))
+		ar += 1
+		for p in cin:
+			ar[p[1], p[0]] = 0
+		print(ar)
+		for i in ar:
+			line = ''
+			for l in i:
+				if l == 0:
+					line += '#'
+				else:
+					line += '_'
+			print(line)
+	except:
+		pass
 
 
 def main():
@@ -46,17 +81,21 @@ def main():
 
 	lastmin = 999999
 	tochk = -1
-	for i in range(0,10):
-		xmax, ymax, tempcoords = Find_Min(coords, i)
-		print(i)
+	for i in range(0,1000000):
+		xmax, ymax = Get_Min(coords)
+		print(i, xmax, ymax)
 		if (xmax + ymax) > lastmin:
-			tochk = i - 1
+			tochk = i# - 1
+			break
 		else:
 			lastmin = xmax + ymax
 
-	xm, ym, coords = Find_Min(coords, tochk)
+	coords = Import_Data(path)
+	coords = Clean_Data(coords)
+	coords = Transform_Plt(coords, tochk)
+	Plot_Points(coords)
 
-	print(coords)
+	#print(coords, xm, ym)
 
 if __name__ == '__main__':
 	main()
